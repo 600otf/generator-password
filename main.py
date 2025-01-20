16df928a26fa6c28c00d9280841ad8f9811d28da
adam-tech-life <cryptosecret1315@proton.me>
2025-01-20 18:04:19 +0100

    new commit

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import base64
 
+import string
+
+
+RED = "\33[91m"
+BLUE = "\33[94m"
+GREEN = "\033[32m"
+YELLOW = "\033[93m"
+PURPLE = '\033[0;35m' 
+CYAN = "\033[36m"
+END = "\033[0m"
+
+banner =f"""
+
+{PURPLE}
+
+  ____    ___  ____     ___  ____    ____  ______   ___   ____       ____    ____   _____ _____ __    __   ___   ____   ___   
+ /    |  /  _]|    \   /  _]|    \  /    ||      | /   \ |    \     |    \  /    | / ___// ___/|  |__|  | /   \ |    \ |   \  
+|   __| /  [_ |  _  | /  [_ |  D  )|  o  ||      ||     ||  D  )    |  o  )|  o  |(   \_(   \_ |  |  |  ||     ||  D  )|    \ 
+|  |  ||    _]|  |  ||    _]|    / |     ||_|  |_||  O  ||    /     |   _/ |     | \__  |\__  ||  |  |  ||  O  ||    / |  D  |
+|  |_ ||   [_ |  |  ||   [_ |    \ |  _  |  |  |  |     ||    \     |  |   |  _  | /  \ |/  \ ||  `  '  ||     ||    \ |     |
+|     ||     ||  |  ||     ||  .  \|  |  |  |  |  |     ||  .  \    |  |   |  |  | \    |\    | \      / |     ||  .  \|     |
+|___,_||_____||__|__||_____||__|\_||__|__|  |__|   \___/ |__|\_|    |__|   |__|__|  \___| \___|  \_/\_/   \___/ |__|\_||_____|  """
+                                                                                                                              
+
+print(banner)
 def generate_password(length, include_digits, include_special_chars):
     # Define possible character sets
     characters = string.ascii_letters
